d520b5a67bb2bf643b01122a61b1082ba920c958
Facu <69550808+Faq-hue@users.noreply.github.com>
2021-08-27 21:53:19 -0300

    Add files via upload

--- ej_ordenamiento/App.java
+++ ej_ordenamiento/App.java
@@ -3,9 +3,14 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
 
+//Archivo   |   Descripcion |   Insercion   |   Shell   |   Quicksort
+//es.txt.gz |   desordenado |     null      |   7103 ms |    1326 ms
+//esi.txt.gz|   invertido   |     null      |   1337 ms |    3402 ms
+//eso.txt.gz|   ordenado    |     null      |   937 ms  |     null
+
 public class App {
     public static void main(String[] args) {
-        String filename = "es.txt.gz";
+        String filename = "eso.txt.gz";
         String line;
         ArrayList<String> words = new ArrayList<>();
         String [] arr;
@@ -21,7 +26,7 @@ public class App {
             arr = words.toArray(new String[0]);
             long start = System.currentTimeMillis(); // para medir el tiempo de ejecución
 
-            sort.shell(arr);
+            sort.quicksort(arr, 0, arr.length-1);
 
             System.out.println("demora: " + (System.currentTimeMillis() - start) + "ms");
 
